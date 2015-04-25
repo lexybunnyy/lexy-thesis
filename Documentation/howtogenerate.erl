81a9a823b13b542db4e479a2b63163774dcc12e9
OEM Configuration (temporary user) <you@example.com>
2015-04-25 10:53:07 +0200

    Documentation: updated

--- Documentation/howtogenerate.erl
+++ Documentation/howtogenerate.erl
@@ -2,6 +2,7 @@ http://www-rohan.sdsu.edu/~aty/bibliog/latex/LaTeXtoPDF.html
 
 pdflatex example.tex
 
+pdflatex fgytvfElteIkThesis.tex
 
 sudo apt-get install texlive texlive-base texlive-base-bin texlive-common texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
 sudo apt-get install texlive texlive-base texlive-base-bin texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
