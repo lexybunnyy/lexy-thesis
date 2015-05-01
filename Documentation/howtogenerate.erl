0db96dda3704319af8df77c0339044ab384fda92
OEM Configuration (temporary user) <you@example.com>
2015-05-01 21:07:27 +0200

    Documentation: update

--- Documentation/howtogenerate.erl
+++ Documentation/howtogenerate.erl
@@ -3,8 +3,11 @@ http://www-rohan.sdsu.edu/~aty/bibliog/latex/LaTeXtoPDF.html
 pdflatex example.tex
 
 ~/git/project/Documentation/generale/
-pdflatex ../fgytvfElteIkThesis.tex
+pdflatex -shell-escape ../fgytvfElteIkThesis.tex
 
 sudo apt-get install texlive texlive-base texlive-base-bin texlive-common texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
 sudo apt-get install texlive texlive-base texlive-base-bin texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
 sudo apt-get install texlive-fonts-recommended latex-beamer texpower texlive-pictures texlive-latex-extra imagemagick
+
+sudo apt-get install texlive-latex-extra python-pygments
+
